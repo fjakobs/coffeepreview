370ef3aa60a79c7b60608e3b1b3a3bcf4d7fbb8d
Fabian Jakobs <fabian.jakobs@web.de>
2011-05-01 11:24:13 -0700

    use coffeescript mode

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 (function() {
 
+    var ace = require("ace/ace");
+    var CoffescriptMode = require("ace/mode/coffee").Mode;
+    
+    var editor = ace.edit("coffeeedit");
+    editor.getSession().setMode(new CoffescriptMode());
+
 })();
