cc5ad94ccbdc57723adacb048ee71afa8c10a905
Fabian Jakobs <fabian.jakobs@web.de>
2011-05-01 11:39:25 -0700

    set js mode

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
     var ace = require("ace/ace");
     var CoffescriptMode = require("ace/mode/coffee").Mode;
+    var JsMode = require("ace/mode/javascript").Mode;
     var Theme = require("ace/theme/twilight");
     
     var editor = ace.edit("coffeeedit");
@@ -11,5 +12,6 @@
     var viewer = ace.edit("jsedit");
     viewer.setTheme(Theme);
     viewer.setReadOnly(true);
+    viewer.getSession().setMode(new JsMode());
     
 })();
