26e59e91f8518fe464c7db9283e655efa539bbd9
Fabian Jakobs <fabian.jakobs@web.de>
2011-05-01 11:26:03 -0700

    set theme for the coffee mode

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
     var ace = require("ace/ace");
     var CoffescriptMode = require("ace/mode/coffee").Mode;
+    var Theme = require("ace/theme/twilight");
+    
     
     var editor = ace.edit("coffeeedit");
     editor.getSession().setMode(new CoffescriptMode());
-
+    editor.setTheme(Theme);
+    
 })();
